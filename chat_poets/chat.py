fa03a65b490aadd108beece448446c0fbc8a8220
Fishead_East <ytzd2696@foxmail.com>
2023-08-14 12:29:52 +0800

    重构代码

--- chat_poets/chat.py
+++ chat_poets/chat.py
@@ -2,22 +2,22 @@
 生成回答
 """
 from LLM.spark_desk import SparkDesk
-from langchain.prompts import PromptTemplate
+from langchain.prompts import PromptTemplate, load_prompt
 from langchain.chains import LLMChain, SimpleSequentialChain
 
 
 class ChatPoet:
     llm = SparkDesk()
 
-    @staticmethod
-    def allow_chat(user_message: str) -> bool:
+    @classmethod
+    def allow_chat(cls, user_message: str) -> bool:
         """
         对话开始时判断用户输入是否合规：提及诗人或古诗
         """
         return user_message == "y"
 
-    @staticmethod
-    def gen_response(pattern: str, history: list[list]) -> str:
+    @classmethod
+    def gen_response(cls, pattern: str, history: list[list]) -> str:
         """
         根据对话历史，产生回答。
         pattern = "adult" | "teen" | "child"，模式
