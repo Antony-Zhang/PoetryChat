a76df7e9dff68bb6960e731ad650025e19cf5849
Fishead_East <ytzd2696@foxmail.com>
2023-08-16 11:29:42 +0800

    debug：读取json文件

--- chat_poets/get_path.py
+++ chat_poets/get_path.py
@@ -12,12 +12,17 @@ def get_prompts_path(filename: str) -> str:
     """
     curPath = os.path.abspath(os.path.dirname(__file__))
     if sys.platform.startswith('win'):
-        rootPath = curPath[:curPath.find("PoetryChat\\") + len("PoetryChat\\")]
-        return rootPath + f"chat_poets\\{filename}"
+        rootPath = curPath[:curPath.find("PoetryChat") + len("PoetryChat")]
+        return rootPath + f"\\chat_poets\\{filename}"
     else:
-        rootPath = curPath[:curPath.find("PoetryChat/") + len("PoetryChat/")]
-        return rootPath + f"chat_poets/{filename}"
+        rootPath = curPath[:curPath.find("PoetryChat") + len("PoetryChat")]
+        return rootPath + f"/chat_poets/{filename}"
 
-if __name__ == '__main__':
-    with open("prompts.json") as f:
-        print(type(json.load(f)))
+
+# if __name__ == '__main__':
+#     # with open("prompts.json") as f:
+#     #     print(type(json.load(f)))
+#     curPath = os.path.abspath(os.path.dirname(__file__))
+#     print("1: " + curPath)
+#     print("2: " + curPath[:curPath.find("PoetryChat") + len("PoetryChat")])
+#     print(get_prompts_path("test.json"))
