715df9aa571407753564d998833cf1c74b4272f5
Fishead_East <ytzd2696@foxmail.com>
2023-08-16 11:38:24 +0800

    debug：读取json文件

--- chat_poets/get_path.py
+++ chat_poets/get_path.py
@@ -11,12 +11,16 @@ def get_prompts_path(filename: str) -> str:
     获取json文件的绝对路径以供访问
     """
     curPath = os.path.abspath(os.path.dirname(__file__))
-    if sys.platform.startswith('win'):
+    # 此处的项目名称需要根据需要更换，若是部署hf则改成“user”
+    if sys.platform.startswith('win'): # win调试
         rootPath = curPath[:curPath.find("PoetryChat") + len("PoetryChat")]
         return rootPath + f"\\chat_poets\\{filename}"
-    else:
+    elif sys.platform.startswith('darwin'): # mac调试
         rootPath = curPath[:curPath.find("PoetryChat") + len("PoetryChat")]
         return rootPath + f"/chat_poets/{filename}"
+    else:  # linux部署
+        rootPath = curPath[:curPath.find("user") + len("user")]
+        return rootPath + f"/user/{filename}"
 
 
 # if __name__ == '__main__':
@@ -24,5 +28,5 @@ def get_prompts_path(filename: str) -> str:
 #     #     print(type(json.load(f)))
 #     curPath = os.path.abspath(os.path.dirname(__file__))
 #     print("1: " + curPath)
-#     print("2: " + curPath[:curPath.find("PoetryChat") + len("PoetryChat")])
+#     print("2: " + curPath[:curPath.find("Z/") + len("PP")])
 #     print(get_prompts_path("test.json"))
