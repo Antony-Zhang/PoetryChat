475eec307ed55f02d7e09db987fdddc82c3ca075
Fishead_East <ytzd2696@foxmail.com>
2023-08-16 11:41:16 +0800

    debug：读取json文件

--- chat_poets/get_path.py
+++ chat_poets/get_path.py
@@ -12,15 +12,15 @@ def get_prompts_path(filename: str) -> str:
     """
     curPath = os.path.abspath(os.path.dirname(__file__))
     # 此处的项目名称需要根据需要更换，若是部署hf则改成“user”
-    if sys.platform.startswith('win'): # win调试
+    if sys.platform.startswith('win'):  # win调试
         rootPath = curPath[:curPath.find("PoetryChat") + len("PoetryChat")]
         return rootPath + f"\\chat_poets\\{filename}"
-    elif sys.platform.startswith('darwin'): # mac调试
+    elif sys.platform.startswith('darwin'):  # mac调试
         rootPath = curPath[:curPath.find("PoetryChat") + len("PoetryChat")]
         return rootPath + f"/chat_poets/{filename}"
     else:  # linux部署
         rootPath = curPath[:curPath.find("user") + len("user")]
-        return rootPath + f"/user/{filename}"
+        return rootPath + f"/{filename}"
 
 
 # if __name__ == '__main__':
